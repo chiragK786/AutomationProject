1f5977a5de2a4dbc1df050488e238d6e64f7b463
chiragK786 <Chiragkhanduja786@gmail.com>
2026-02-07 12:07:23 +0530

    "intial commit"

---
+++ src/test/java/com/ui/test/login/SigninTest.java
@@ -0,0 +1,22 @@
+package com.ui.test.login;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SigninTest {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriver wd = new ChromeDriver(); // Launch a browser window !! Browser Session is Created...
+		wd.get("https://automationpractice.techwithjatin.com"); // Go to Website
+		wd.manage().window().maximize(); //Maximize the Browser
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
